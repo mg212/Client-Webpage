4d9f0abb50c183d7a33d51b5def187fe0e52220e
Max Goncharov <mgonchar@live.com>
2019-06-23 23:58:10 -0400

    Initial Commit

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+// Client Webpage
+// email address validation
+
+let button = document.getElementById('button');
+button.addEventListener('click', runEvent);
+
+function runEvent(e) {
+    e.preventDefault();
+//
+// alert regarding subscription 
+    alert('Thanks for subscribing!');
+}
+
+
+
+
+
+
+
